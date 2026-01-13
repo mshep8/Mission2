d53202bbd55ae55ecc81d21d649fbb36781d126c
mshep8 <marycatherineshepherd@gmail.com>
2026-01-13 12:04:05 -0700

    Main code for the assignment.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+namespace Mission2;
+
+using static System.Console;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate? ");
+        int rolls = int.Parse(Console.ReadLine());
+
+        Simulation sim = new Simulation();
+        int[] results = sim.RunSimulation(rolls);
+        
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each '*' represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {rolls}.\n");
+
+        for (int sum = 2; sum <= 12; sum++)
+        {
+            int percent = results[sum] * 100 / rolls;
+            Console.Write($"{sum}: ");
+
+            for (int count = 0; count < percent; count++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
--- Simulation.cs
+++ Simulation.cs
@@ -2,5 +2,19 @@ namespace Mission2;
 
 public class Simulation
 {
-    
+    public int[] RunSimulation(int rolls)
+    {
+        int[] results = new int[13];
+        Random random = new Random();
+
+        for (int count = 0; count < rolls; count++)
+        {
+            int die1 = random.Next(1, 7);
+            int die2 = random.Next(1, 7);
+
+            int sum = die1 + die2;
+            results[sum]++;
+        }
+        return results;
+    }
 }
